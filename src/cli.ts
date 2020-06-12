82703ec93b8e2a46fa18f24d110b2dedb6562963
Frederik Hanghøj Iversen <fiversen@zendesk.com>
2020-06-12 14:52:56 +0200

    Support multiple file-names as params

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@ import {writeFileSync} from 'fs'
 import path from 'path'
 import {homedir} from 'os'
 import * as yargs from 'yargs'
+import * as Types from './types'
 
 import {ts} from 'ts-morph'
 
@@ -9,32 +10,26 @@ import extractFunctions from './index'
 
 // .option('directory', {type: 'positional'})
 
-const cli = yargs
-  .option('out', {
-    // TODO: figure out how to show the description on --help
-    type: 'string',
-    description: 'Out file path. If missing will write to stdout',
-    hidden: false,
-  })
+const cli = yargs.option('out', {
+  // TODO: figure out how to show the description on --help
+  type: 'string',
+  description: 'Out file path. If missing will write to stdout',
+  hidden: false,
+})
 
 const normalize = (p: string) => path.normalize(p.replace(/^~/, homedir()))
 
 function main() {
-  const {_: fileNames, out} = cli.argv
+  const {_: directories, out} = cli.argv
 
-  if (fileNames.length !== 1) {
-    cli.showHelp()
-    process.exit(1)
-  }
-
-  const [directory] = fileNames
-
-  const results = extractFunctions(normalize(directory), {
-    compilerOptions: {
-      target: ts.ScriptTarget.ES5,
-      module: ts.ModuleKind.CommonJS,
-    },
-  })
+  const results: Types.FunctionRecord[] = directories.flatMap(directory =>
+    extractFunctions(normalize(directory), {
+      compilerOptions: {
+        target: ts.ScriptTarget.ES5,
+        module: ts.ModuleKind.CommonJS,
+      },
+    }),
+  )
 
   if (!out) {
     console.log(JSON.stringify(results))
